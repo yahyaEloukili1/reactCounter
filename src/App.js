3019e753b8777a36b9f6d49d818ff72acefb3d92
yahya <yahya.eloukili11@gmail.com>
2021-06-11 23:58:06 +0100

    first commit, class component, props, state, and setState

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import  Counter from "./Counter";
+import  React from "react";
+
+function App() {
+  return (
+    <Counter initialCount={0}/>
+     
+  )
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count : props.initialCount
+        }
+
+    }
+    changeCount(amount){
+        this.setState(prevState=>{
+            return {count : prevState.count + amount}
+        })
+        this.setState(prevState=>{
+            return {count : prevState.count + amount}
+        })
+       
+    }
+    render() {
+        return (
+            <>
+                <button onClick={()=>this.changeCount(-1)}>-</button>
+                <span>{this.state.count}</span>
+                <button onClick={()=>this.changeCount(+1)}>+</button>
+           </>
+        )
+    }
+}
